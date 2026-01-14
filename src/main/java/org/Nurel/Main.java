bd7d5b73ff67d7bc137e1d524ad766126ec9714a
Nurel <nazi070582@gmail.com>
2026-01-14 14:09:10 +0600

    Initial commit

---
+++ src/main/java/org/Nurel/Main.java
@@ -0,0 +1,8 @@
+package org.Nurel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Система SmartReserve запускается...");
+        System.out.println("Готов к работе!");
+    }
+}
